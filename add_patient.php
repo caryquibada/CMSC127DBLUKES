ee49d6d125759e46f7945d09245ab254d2af5aa7
caryquibada <31432885+caryquibada@users.noreply.github.com>
2017-09-16 13:31:38 +0800

    basic mysql and php interaction

---
+++ add_patient.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Insert Patient Data to MYSQL Database Using PHP</title>
+</head>
+<body>
+	<form action="add.php" method="post">
+		First Name: <input type="text" name="firstname">
+		<br/>
+		Last Name: <input type="text" name="lastname">
+		<br/>
+		<input type="submit" value="Insert">
+	</form>
+</body>
+</html>

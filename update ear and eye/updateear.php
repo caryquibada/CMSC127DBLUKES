125440e408d5652493cc385b0d918618ebe0f6de
Boogertastic <31267041+Boogertastic@users.noreply.github.com>
2017-10-26 02:45:58 +0800

    Add files via upload

---
+++ update ear and eye/updateear.php
@@ -0,0 +1,31 @@
+<?php
+    $connect = mysqli_connect('localhost','root','');
+    $id=$_GET['id'];
+	if(!$connect){
+		echo 'No connection to server';
+	}
+	if(!mysqli_select_db($connect,'lukedb')){
+		echo 'Database "lukedb" is not selected';
+    }
+	$rem=$_POST['earremark'];
+	$lear=$_POST['leftear'];
+    $rear=$_POST['rightear'];
+    $sql="UPDATE ear SET ear_remark ='$rem' where patient_id=$id";
+    if(!mysqli_query($connect,$sql)){
+		echo 'Insert failure';
+    }  
+    $sql="UPDATE ear SET left_ear='$lear' where patient_id=$id";
+    if(!mysqli_query($connect,$sql)){
+		echo 'Insert failure';
+    }  
+    $sql="UPDATE ear SET right_ear='$rear' where patient_id=$id";
+    if(!mysqli_query($connect,$sql)){
+		echo 'Insert failure';
+	}
+    else{
+        echo '<script language="javascript" type="text/javascript"> 
+		alert("Patient Successfully Updated");
+        </script>';
+        header("refresh:0.2; url=displayEar.php?id=$id");
+	}
+?>
